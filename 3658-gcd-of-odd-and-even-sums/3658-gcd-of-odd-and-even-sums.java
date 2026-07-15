c0bbc9bca2349eeafc05a3f858199d7a1a860f41
Miryala achyuth kumar <2400030005@kluniversity.in>
2026-07-15 10:07:16 +0530

    Time: 1 ms (83.11%), Space: 42.7 MB (39.85%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,28 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int en = 2*n;
-        int on = 2*n-1;
-
-        int esum = 0;
-        int osum = 0;
-        for(int i = 1;i<=on;i+=2)
-        {
-              osum+=i;
-        }
-        for(int i = 2;i<=en;i+=2)
-        {
-              esum+=i;
-        }
-        
-        int min = Math.min(esum,osum);
-        int ans = 0;
-        for(int i =1 ;i<=min;i++)
-        {
-            if(esum%i == 0 && osum%i ==0)
-            {
-                ans = Math.max(i,ans);
-            }
-        }
-    return ans;
+        return n;
     }
 }
