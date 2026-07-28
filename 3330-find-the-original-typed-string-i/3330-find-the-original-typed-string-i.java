689c9c0968e0ef964b3074e5ce7f4c43dfe3cfbb
Miryala achyuth kumar <2400030005@kluniversity.in>
2026-07-28 19:26:18 +0530

    Time: 1 ms (85.71%), Space: 43.5 MB (27.3%) - LeetHub

---
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int sum = 1;
+        int n = word.length();
+        for(int i = 0;i<n-1;i++)
+        {
+            if(word.charAt(i) == word.charAt(i+1))
+            {
+                sum++;
+            }
+        }
+        return sum;
+
+    }
+}
