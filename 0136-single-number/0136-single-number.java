b3cb7ca656572aa29c0f2aad30236c09b2ee1e3b
Miryala achyuth kumar <2400030005@kluniversity.in>
2026-07-31 15:41:21 +0530

    Time: 10 ms (27.46%), Space: 47.3 MB (6.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+       int x = 0;
+        for(int i = 0;i<n;i++)
+        {
+            x ^=nums[i];
+        }
+        return x;
+    }
+}
