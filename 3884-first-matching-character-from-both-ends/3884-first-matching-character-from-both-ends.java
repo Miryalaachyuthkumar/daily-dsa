da5382a5bf1d10f0fe56a71dd31709a05f216851
Miryala achyuth kumar <2400030005hte@gmail.com>
2026-09-02 19:42:03 +0530

    Time: 0 ms (100%), Space: 43.3 MB (68.89%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstMatchingIndex(String s) {
+        int l = 0;
+        int r = s.length()-1;
+        while(l<=r)
+        {
+           
+           if(s.charAt(l) == s.charAt(r))
+           {
+             return l;
+           }
+           else
+           {
+             l++;
+             r--;
+           }
+        }
+        return -1;
+    }
+}
